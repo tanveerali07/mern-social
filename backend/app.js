3803ff39820d3ff2acf9551c9985df59e377d20a
tanveerali07 <142293698+tanveerali07@users.noreply.github.com>
2023-08-15 12:13:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,10 @@ app.use("/api/v1", user);
 
 app.use(cors({ origin: "*" }));
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "/opt/render/project/src/frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "/opt/render/project/src/frontend/build/index.html"));
 });
 
 module.exports = app;
